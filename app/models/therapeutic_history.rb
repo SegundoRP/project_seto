3f1339f841e3976cbb9bd9e8fbf5a3ec7d58ec7b
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-06-28 23:12:46 -0500

    pass test therapeutics history

--- app/models/therapeutic_history.rb
+++ app/models/therapeutic_history.rb
@@ -2,4 +2,5 @@ class TherapeuticHistory < ApplicationRecord
   belongs_to :patient
 
   validates :first_time_attention, inclusion: { in: [true, false] }
+  validates :events_in_life, :familiar_antecedent, presence: true
 end
