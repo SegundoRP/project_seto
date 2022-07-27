54f82f3d365612f00960be83a0c166c931a9df76
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-07-27 01:08:58 -0500

    create index show and set patient actions

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,11 +1,11 @@
 class PatientsController < ApplicationController
+  before_action :set_patient, only: %i[show]
 
   def index
-
+    @patients = Patient.all
   end
 
   def show
-
   end
 
   def new
@@ -26,5 +26,11 @@ class PatientsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def set_patient
+    @patient = Patient.find(params[:id])
   end
 end
