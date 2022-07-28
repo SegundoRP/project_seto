e33f876c2d1bc754e3313fd7a09a5a45bdfdf196
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-07-27 23:34:43 -0500

    complete patient controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,6 @@
 class PatientsController < ApplicationController
   before_action :set_patient, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!
 
   def index
     @patients = Patient.all
@@ -13,23 +14,42 @@ class PatientsController < ApplicationController
   end
 
   def create
+    @patient = Patient.new(patient_params)
 
+    if @patient.save
+      redirect_to patient_path(@patient)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
-
+    if @patient.update(patient_params)
+      redirect_to patient_path(@patient)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @patient.destroy
 
+    respond_to do |format|
+      format.html { redirect_to patients_path, notice: 'El paciente fue eliminado exitosamente' }
+    end
   end
 
   private
 
   def set_patient
     @patient = Patient.find(params[:id])
+  end
+
+  def patient_params
+    params.require(:patient).permit(:name, :last_name, :age, :email, :phone, :address, :civil_state,
+                                    :children, :instruction_grade, :occupation)
   end
 end
