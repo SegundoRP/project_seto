a9cedb26620ab85fa8ba8c814ab7887488aa4b39
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-08-09 21:32:44 -0500

    create consultation controller with show and index

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,2 +1,34 @@
 class ConsultationsController < ApplicationController
+  before_action :set_patient, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!
+
+  def index
+    @consultations = Consultation.all
+  end
+
+  def show
+    @consultation =
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_consultation
+
+  end
+
+  def consultation_params
+    params.require(:consultation).permit(:date, :status, :summary, :reason_consultation, :doctor_id, :patient_id, :type_of_service_id)
+  end
 end
