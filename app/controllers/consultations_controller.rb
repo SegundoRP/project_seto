1e3a310175805455e87eb68440903b48284d5e26
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-08-09 21:50:53 -0500

    finish crud consultation controller

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,5 +1,5 @@
 class ConsultationsController < ApplicationController
-  before_action :set_patient, only: %i[show edit update destroy]
+  before_action :set_consultation, only: %i[show edit update destroy]
   skip_before_action :authenticate_user!
 
   def index
@@ -7,25 +7,45 @@ class ConsultationsController < ApplicationController
   end
 
   def show
-    @consultation =
+  end
+
+  def new
+    @consultation = Consultation.new
   end
 
   def create
+    @consultation = Consultation.new(consultation_params)
 
+    if @consultation.save
+      redirect_to consultation_path(@consultation)
+    else
+      render :new
+    end
+  end
+
+  def edit
   end
 
   def update
-
+    if @consultation.update(consultation_params)
+      redirect_to consultation_path(@consultation)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @consultation.destroy
 
+    respond_to do |format|
+      format.html { redirect_to consultations_path, notice: 'La consulta fue eliminada exitosamente' }
+    end
   end
 
   private
 
   def set_consultation
-
+    @consultation = Consultation.find(params[:id])
   end
 
   def consultation_params
