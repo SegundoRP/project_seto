af91799e35587941703808e85a268b676f82b556
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-08-17 23:37:32 -0500

    nested attributes in patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,6 +3,8 @@ class Patient < ApplicationRecord
   has_many :diagnostics
   has_one :therapeutic_history
 
+  accepts_nested_attributes_for :consultations
+
   # validates :first_name, :last_name, :age, :email, :phone, :address, presence: true
 
   def fullname
