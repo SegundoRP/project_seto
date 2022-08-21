73a696330b9f15d3f426a1ba00975d6cc4eed358
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-08-21 18:04:43 -0500

    modify nested attributes

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -11,17 +11,18 @@ class PatientsController < ApplicationController
 
   def new
     @patient = Patient.new
+    @patient.consultations.new
   end
 
   def create
     @patient = Patient.new(patient_params)
-    respond_to do |format|
+    # respond_to do |format|
       if @patient.save
-        format.html { redirect_to patient_path(@patient), notice: 'El paciente fue creado exitosamente' }
+        redirect_to patient_path(@patient), notice: 'El paciente fue creado exitosamente'
       else
         render :new
       end
-    end
+    # end
   end
 
   def edit
