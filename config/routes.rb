57a2459aaba567e427cfbc5d73ffcfe70bb77457
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-08-24 21:35:02 -0500

    add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/dashboard', to: 'pages#dashboard'
-  resources :patients
+  resources :patients do
+    resources :diagnostics
+  end
   resources :consultations
 end
