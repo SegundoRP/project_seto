a73810e983e9a9779ca741dd8e5ff123d8f9c8de
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-08-27 18:08:56 -0500

    finish treatment controller

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -1,20 +1,42 @@
 class TreatmentsController < ApplicationController
-  before_action :set_treatment, only: %i[show]
+  before_action :set_treatment, only: %i[show update destroy]
   skip_before_action :authenticate_user!
 
   def show
   end
 
-  def create
+  def new
+    @treatment = Treatment.new
+  end
 
+  def create
+    @treatment = Treatment.new(treatment_params)
+
+    respond_to do |format|
+      if @treatment.save
+        format.html { redirect_to treatment_path(@treatment), notice: 'El tratamiento fue creado exitosamente' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def update
-
+    respond_to do |format|
+      if @treatment.update(treatment_params)
+        format.html { redirect_to treatment_path(@treatment), notice: 'El tratamiento fue actualizado exitosamente' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def destroy
+    @treatment.destroy
 
+    respond_to do |format|
+      format.html { redirect_to :new, notice: 'El tratamiento fue eliminado exitosamente' }
+    end
   end
 
   private
