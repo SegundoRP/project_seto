fd6854e712d43fc9c9d05eb5a06aefcb7035375d
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-08-29 22:11:36 -0500

    add strong params, new action and show

--- app/controllers/therapeutics_history_controller.rb
+++ app/controllers/therapeutics_history_controller.rb
@@ -1,3 +1,25 @@
-class TreatmentsController < ApplicationController
+class TherapeuticsHistoryController < ApplicationController
+  before_action :set_therapeutics_history, only: %i[show]
 
+  def show
+  end
+
+  def new
+    @therapeutics_history = TherapeuticsHistory.new
+  end
+
+  def create
+
+  end
+
+  private
+
+  def set_therapeutics_history
+    @therapeutics_history = TherapeuticsHistory.find(params[:id])
+  end
+
+  def therapeutics_history_params
+    params.require(:therapeutics_history).permit(:first_time_attention, :previous_diagnostic, :events_in_life,
+                                                 :previous_treatment, :familiar_antecedent)
+  end
 end
