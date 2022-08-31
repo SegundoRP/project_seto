5903c7e6533edb8bcfe618ffb726749417dbdc88
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-08-30 22:21:11 -0500

    create type of service controller

---
+++ app/controllers/type_of_services_controller.rb
@@ -0,0 +1,39 @@
+class TypeOfServicesController < ApplicationController
+  before_action :set_type_of_service, only: %i[show update destroy]
+  before_action :authenticate_user!
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_type_of_service
+
+  end
+
+  def type_of_service_params
+
+  end
+
+end
