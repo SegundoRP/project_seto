04f27043475d99a9932e0efa8cfc57b7a23b5396
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-08-31 22:41:46 -0500

    add destroy and index type of service controller

--- app/controllers/type_of_services_controller.rb
+++ app/controllers/type_of_services_controller.rb
@@ -3,11 +3,10 @@ class TypeOfServicesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
+    @type_of_services = TypeOfService.all
   end
 
   def show
-
   end
 
   def new
@@ -23,13 +22,17 @@ class TypeOfServicesController < ApplicationController
   end
 
   def destroy
-
+    respond_to do |format|
+      if @type_of_service.destroy
+        format.html { redirect_to type_of_services_path, notice: 'El tipo de servicio fue eliminado' }
+      end
+    end
   end
 
   private
 
   def set_type_of_service
-
+    @type_of_service = TypeOfService.find(params[:id])
   end
 
   def type_of_service_params
