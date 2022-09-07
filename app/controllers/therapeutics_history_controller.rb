40907a599c35f6be0bfc43e1427b7a9463dfd66e
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-09-06 23:05:41 -0500

    update therapeutics controller

--- app/controllers/therapeutics_history_controller.rb
+++ app/controllers/therapeutics_history_controller.rb
@@ -1,5 +1,6 @@
 class TherapeuticsHistoryController < ApplicationController
   before_action :set_therapeutics_history, only: %i[show update destroy]
+  skip_before_action :authenticate_user!
 
   def show
   end
@@ -15,7 +16,7 @@ class TherapeuticsHistoryController < ApplicationController
       if @therapeutics_history.save
         format.html { redirect_to therapeutics_history_path(@therapeutics_history), notice: 'La historia terapeútica ha sido creada' }
       else
-        format.html { render :new }
+        format.html { render :new, alert: flash.now[:alert] = 'La historia terapeútica no puso ser creada' }
       end
     end
   end
@@ -25,14 +26,18 @@ class TherapeuticsHistoryController < ApplicationController
       if @therapeutics_history.update(therapeutics_history_params)
         format.html { redirect_to therapeutics_history_path(@therapeutics_history), notice: 'La historia terapeútica ha sido actualizada' }
       else
-        format.html { render :new }
+        format.html { render :new, alert: flash.now[:alert] = 'La historia terapeútica no pudo ser actualizada' }
       end
     end
   end
 
   def destroy
-    if @therapeutics_history.destroy
-      format.html { redirect_to :new, notice: 'La historia terapeútica fue eliminada' }
+    respond_to do |format|
+      if @therapeutics_history.destroy
+        format.html { redirect_to :new, notice: 'La historia terapeútica fue eliminada' }
+      else
+        format.html { render :show, alert: flash.now[:alert] = 'La historia terapeútica no pudo ser eliminada' }
+      end
     end
   end
 
