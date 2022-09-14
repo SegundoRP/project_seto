1df8b297f474015f08d8b35339548cf69216cf0d
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-09-13 23:20:50 -0500

    add allows destroy and reject if patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,7 +3,7 @@ class Patient < ApplicationRecord
   has_many :diagnostics
   has_one :therapeutic_history
 
-  accepts_nested_attributes_for :consultations
+  accepts_nested_attributes_for :consultations, allow_destroy: true, reject_if: :all_blank
 
   validates :first_name, :last_name, :age, :email, :phone, :address, presence: true
 
