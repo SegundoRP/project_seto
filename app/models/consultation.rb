a960b2d6683bc1cb9cf6e8dd3ef8b7036beea6e7
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-10-03 23:57:48 -0500

    add more validations in consultations

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -3,5 +3,6 @@ class Consultation < ApplicationRecord
   belongs_to :patient
   belongs_to :type_of_service
 
+  validates :date, :status, :summary, :reason_consultation, presence: true
   validates :date, :status, :summary, :reason_consultation, presence: { message: 'Debe estar presente' }
 end
