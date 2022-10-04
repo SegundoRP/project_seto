24f90b4e8097d99c674bb84cbab2b5be627da191
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-10-04 00:11:19 -0500

    added inverse of

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,8 +1,8 @@
 class Consultation < ApplicationRecord
-  belongs_to :doctor
-  belongs_to :patient
+  belongs_to :doctor, inverse_of: :consultations
+  belongs_to :patient, inverse_of: :consultations
   belongs_to :type_of_service
 
-  validates :date, :status, :summary, :reason_consultation, presence: true
+  validates :date, :status, :summary, :reason_consultation, :doctor, :patient, presence: true
   validates :date, :status, :summary, :reason_consultation, presence: { message: 'Debe estar presente' }
 end
