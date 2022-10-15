8f6c790ff169a6e3c40b597f96d7bdb640050861
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-10-14 21:36:45 -0500

    added relationship in patients

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,6 +2,7 @@ class Patient < ApplicationRecord
   has_many :consultations
   has_many :diagnostics
   has_one :therapeutic_history
+  has_many :doctors, through: :consultations
 
   accepts_nested_attributes_for :consultations, reject_if: :all_blank?, allow_destroy: true
 
