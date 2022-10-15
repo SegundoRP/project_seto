687e44572b8cba872fba808529c4b159290b18ad
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-10-14 21:35:46 -0500

    added relationship in doctor

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,6 +1,6 @@
 class Doctor < ApplicationRecord
   belongs_to :user
   has_many :consultations
-
+  has_many :patients, through: :consultations
   # validates :first_name, :last_name, :age, :profession, :specialty, :college_number, :user_id, presence: true
 end
