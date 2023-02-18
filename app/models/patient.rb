5d71a3aae1f36e175e5788645a5eeb1fc667dc1f
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-02-17 22:03:10 -0500

    deleted method in model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -7,8 +7,4 @@ class Patient < ApplicationRecord
   accepts_nested_attributes_for :consultations, reject_if: :all_blank?, allow_destroy: true
 
   validates :first_name, :last_name, :age, :email, :phone, :address, presence: true
-
-  def fullname
-    "#{first_name} #{last_name}"
-  end
 end
